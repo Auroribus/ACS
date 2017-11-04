93e2fa8830f0fbbaa1b97da7eb5805a194a4dee5
StevenDijcks <stief_27@hotmail.com>
2017-11-04 19:48:08 +0100

    adding Azure SQL server + DB

--- Web Application/CardStocks/CardStocks/Program.cs
+++ Web Application/CardStocks/CardStocks/Program.cs
@@ -1,12 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using System.Data.SqlClient;
+using System.Text;
 
 namespace CardStocks
 {
@@ -14,12 +8,46 @@ namespace CardStocks
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
-        }
+      try
+      {
+        SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+        builder.DataSource = "cardstocks-ap.database.windows.net";
+        builder.UserID = "steven";
+        builder.Password = "8/Fe/!1QlY#";
+        builder.InitialCatalog = "CardStocksDB";
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+        using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+        {
+          Console.WriteLine("\nQuery data example:");
+          Console.WriteLine("=========================================\n");
+
+          connection.Open();
+          StringBuilder sb = new StringBuilder();
+          sb.Append("SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName ");
+          sb.Append("FROM [SalesLT].[ProductCategory] pc ");
+          sb.Append("JOIN [SalesLT].[Product] p ");
+          sb.Append("ON pc.productcategoryid = p.productcategoryid;");
+          String sql = sb.ToString();
+
+          using (SqlCommand command = new SqlCommand(sql, connection))
+          {
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+              while (reader.Read())
+              {
+                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+              }
+            }
+          }
+        }
+      }
+      catch (SqlException e)
+      {
+        Console.WriteLine(e.ToString());
+      }
+      Console.ReadLine();
     }
+  }
+    
 }
+
