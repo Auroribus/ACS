48d92db2f99b6afc4d1ef397829fc383237a9d4c
TdeNooijer <tom@laservice.nl>
2017-12-19 20:45:00 +0100

    merge

--- Web Application/CardStocks/CardStocks/Data/CSContext.cs
+++ Web Application/CardStocks/CardStocks/Data/CSContext.cs
@@ -3,24 +3,25 @@ using CardStocks.Models;
 
 namespace CardStocks.Models
 {
-    public class CSContext: DbContext
+  public class CSContext : DbContext
+  {
+    public CSContext(DbContextOptions<CSContext> options) : base(options)
     {
-        public CSContext(DbContextOptions<CSContext> options):base(options)
-        {
-            
-        }
 
-        public DbSet<Card> Cards { get; set; }
-        public DbSet<Collection> Collections { get; set; }
-        public DbSet<BuyModel> BuyListings { get; set; }
-        public DbSet<SellModel> SellListings { get; set; }
-        public DbSet<UserModel> UserList { get; set; }
-        public DbSet<Orders> Orders { get; set; }
-        public DbSet<WishList> WishLists { get; set; }
-        public DbSet<WishCard> WishCards { get; set; }
+    }
 
-        public DbSet<Membership> Membership { get; set; }
-        public DbSet<CardStocks.Models.Chatroom> Chatroom { get; set; }
-        public DbSet<CardStocks.Models.Message> Message { get; set; }
+    public DbSet<Card> Cards { get; set; }
+    public DbSet<Collection> Collections { get; set; }
+    public DbSet<BuyModel> BuyListings { get; set; }
+    public DbSet<SellModel> SellListings { get; set; }
+    public DbSet<UserModel> UserList { get; set; }
+    public DbSet<Orders> Orders { get; set; }
+    public DbSet<WishList> WishLists { get; set; }
+    public DbSet<WishCard> WishCards { get; set; }
 
+    public DbSet<Membership> Membership { get; set; }
+    public DbSet<CardStocks.Models.Chatroom> Chatroom { get; set; }
+    public DbSet<CardStocks.Models.Message> Message { get; set; }
+
+  }
 }
